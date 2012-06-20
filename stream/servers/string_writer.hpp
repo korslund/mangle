aae8e527737303dcedd5c67c7e13053310910a57
Nicolay Korslund <korslund@gmail.com>
2012-06-20 12:47:51 +0200

    StringWriter stream

---
+++ stream/servers/string_writer.hpp
@@ -0,0 +1,41 @@
+#ifndef MANGLE_STREAM_STRINGWRITER_H
+#define MANGLE_STREAM_STRINGWRITER_H
+
+#include "../stream.hpp"
+#include <string>
+
+namespace Mangle {
+namespace Stream {
+
+/** A stream that writes to an std::string. You provide the string
+    yourself, so if you are writing large amounts of data you can
+    optimize by calling string::reserve() first.
+
+    The data is appended to any existing data in the string.
+ */
+class StringWriter : public Stream
+{
+  std::string &out;
+
+ public:
+  StringWriter(std::string &_out)
+    : out(_out);
+  {
+    isSeekable = false;
+    hasPosition = false;
+    hasSize = false;
+    isWritable = true;
+    isReadable = false;
+  }
+
+  size_t write(const void* buf, size_t len)
+  {
+    out.append((const char*)buf, len);
+    return len;
+  }
+};
+
+typedef boost::shared_ptr<StringWriter> StringWriterPtr;
+
+}} // namespaces
+#endif
